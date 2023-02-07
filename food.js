8b005df677aee7aab7ca4d0ef8dd92ad0dfd93f0
SHRI OM TIWARI <shriomtiwari3228@gmail.com>
2023-02-07 14:27:03 -0800

    Welcome to ZeroPaisa

---
+++ food.js
@@ -0,0 +1,38 @@
+// console.log("This Is For Loop in JS")
+
+// let i = 0;
+// for(i=0; i<3; i++)
+// {
+//     console.log(i);
+// }
+
+
+let friends = ["SHRI","ROHAN", "RITIK", "MUKESH"];
+// for (let index = 0; index < friends.length; index++) {
+//      element = friends[index];
+//      console.log("HELLO,  " + friends[index]);
+//       }
+
+
+// alert("Harry bhai Uploading soon the Crazy JS tutorial");
+
+// let cwh = confirm("Harry bhai jaldi kro mere bhai i m waiting ") ;
+// console. log (cwh) ;
+
+// if(cwh) {
+// console. log ("Are You uploading the Tutorial🙂 " ) ;
+// } 
+// else{
+// console. log ("I m waiting some time for the crazy JS Tutorial 🥰" ) ;
+// } 
+
+
+ alert("Welcome To The Garib Brand: *zeroPaisa*");
+
+
+/*let text;
+if (confirm("Press a button!") == true) {
+  text = "You pressed OK!";
+} else {
+  text = "You canceled!";
+}*/
